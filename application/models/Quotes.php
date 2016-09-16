695d0145f87f60ff365b9fa2ce7ba329d805e38a
Thach Le <A00926906@ad.bcit.ca>
2016-09-16 12:26:56 -0700

    Added avatar one, and a quote to quotes.php
    Signed-off-by: Thach Le <A00926906@ad.bcit.ca>

--- application/models/Quotes.php
+++ application/models/Quotes.php
@@ -21,8 +21,11 @@ class Quotes extends CI_Model {
 			'what' => 'How do you get a sweet little 80-year-old lady to say the F word? Get another sweet little 80-year-old lady to yell “BINGO!”'),
 		array('id' => '5', 'who' => 'Socrates', 'mug' => 'socrates-150x150.jpg', 'where' => '/wise/bingo',
 			'what' => 'By all means, marry. If you get a good wife, you’ll become happy; if you get a bad one, you’ll become a philosopher.'),
-		array('id' => '6', 'who' => 'Isaac Asimov', 'mug' => 'isaac-asimov-150x150.jpg', 'where' => '/comp4711/wisdom',
+		array('id' => '6', 'who' => 'BOB MONKHOUSE', 'mug' => 'one.jpg', 'where' => '/broken',
+			'what' => 'When I die, I want to go peacefully like my grandfather did–in his sleep. Not yelling and screaming like the passengers in his car.'),
+		array('id' => '7', 'who' => 'Isaac Asimov', 'mug' => 'isaac-asimov-150x150.jpg', 'where' => '/comp4711/wisdom',
 			'what' => 'Those people who think they know everything are a great annoyance to those of us who do.')
+	
 	);
 
 	// Constructor
